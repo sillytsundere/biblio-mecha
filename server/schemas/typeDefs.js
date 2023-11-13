c54f5c37e3e7f303b96768e95997028324b82795
Paige <paigehcarroll@gmail.com>
2023-11-13 13:54:47 -0600

    wrote the resolvers file, added input type to ease organization and use of savebook mutation

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    input savedBook {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+    type Query {
+        me: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(input: savedBook): User
+        removeBook(bookId: ID!): User
+    }
+`
+
+module.exports = typeDefs;
