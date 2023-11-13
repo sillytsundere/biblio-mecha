09cde5eeadf3992d061c09b6f955bcfcb8acc554
Paige <paigehcarroll@gmail.com>
2023-11-13 10:26:06 -0600

    updated the server auth file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,17 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
-
+ // if no token passed, return the request
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -28,11 +28,14 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // return the req object
+    // which is then passed to the resolver as 'context'
+    return req;
+    // // send to next endpoint
+    // next();
   },
   signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+    const payload = { email, username, _id };
 
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
